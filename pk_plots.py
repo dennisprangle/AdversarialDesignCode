708950a21a4736d614395730458c7e2e857c6ad1
dennisprangle <dennis.prangle@gmail.com>
2020-08-06 08:51:35 +0100

    Update PK example and summaries

--- pk_plots.py
+++ pk_plots.py
@@ -1,47 +1,94 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import pickle
-plt.ion()
 
 with open('pk_example.pkl', 'rb') as infile:
-    out = pickle.load(infile)
+    out_GDA, out_SGD = pickle.load(infile)
 
-output_list, final_design_list, point_exchange_time_list = zip(*out)
+output_GDA_list, final_design_GDA_list, pe_time_GDA_list = zip(*out_GDA)
+output_SGD_list, final_design_SGD_list, pe_time_SGD_list = zip(*out_SGD)
 
-output = output_list[0]
+##################
+## EXECUTION TIMES
+##################
 
-plt.figure()
+gda_run_times = [o['time'][-1] for o in output_GDA_list]
+mean_gda_run_time = np.mean(np.array(gda_run_times))
+print('Mean GDA run time {:.1f}s'.format(mean_gda_run_time))
+sgd_run_times = [o['time'][-1] for o in output_SGD_list]
+mean_sgd_run_time = np.mean(np.array(sgd_run_times))
+print('Mean SGD run time {:.1f}s'.format(mean_sgd_run_time))
+
+mean_gda_pe_run_time = np.mean(np.array(pe_time_GDA_list))
+print('Mean GDA point exchange run time {:.1f}s'.format(mean_gda_pe_run_time))
+mean_pe_run_time = np.mean(np.array(pe_time_SGD_list))
+print('Mean SGD point exchange run time {:.1f}s'.format(mean_sgd_run_time))
+
+##############
+## TRACE PLOTS
+##############
+
+output = output_GDA_list[0]
+
+fig_tau = plt.figure()
 out_design = output['design']
 for i in range(out_design.shape[1]):
   plt.plot(output['iterations'], out_design[:,i])
-
 plt.xlabel('Iterations')
 plt.ylabel('Observation time')
 plt.tight_layout()
+plt.savefig('plots/design_trace.pdf')
 
-plt.figure()
+fig_A = plt.figure()
 out_A = output['A']
 out_A = out_A.reshape((out_A.shape[0], -1))
 for i in range(out_A.shape[1]):
   plt.plot(output['iterations'], out_A[:,i])
-
 plt.xlabel('Iterations')
 plt.ylabel('A matrix')
 plt.tight_layout()
+plt.savefig('plots/A_trace.pdf')
 
-plt.figure()
-plt.plot(output['iterations'], output['objectiveK'])
+fig_K = plt.figure()
+for output in output_GDA_list:
+    plt.plot(output['iterations'], output['objectiveK'], color='b', alpha=0.2)
 plt.xlabel('Iterations')
 plt.ylabel('K objective')
 plt.yscale('symlog')
 plt.tight_layout()
+plt.savefig('plots/K_trace.pdf')
 
-plt.figure()
-for output in output_list:
-    plt.plot(output['iterations'], output['objectiveJ'])
+fig_J = plt.figure()
+for output in output_GDA_list:
+    plt.plot(output['iterations'], output['objectiveJ'], color='b', alpha=0.2)
 plt.xlabel('Iterations')
 plt.ylabel('J objective')
 plt.tight_layout()
+plt.savefig('plots/J_trace.pdf')
 
-wait = input('Press enter to terminate')
+#####################
+## OUTPUT DESIGN PLOTS
+#####################
 
-## TO DO: SAVE OUTPUTS (TO A FOLDER), USE SOPHIE'S CODE FOR J OBJECTIVE PLOT APPEARANCE
+plt.figure()
+gda_designs = [np.sort(o['design'][-1,:]) for o in output_GDA_list]
+design_index = [i+1 for i in range(15)]
+#Commented out lines do a line plot instead
+#gda_designs = np.vstack(gda_designs)
+#for i in range(100):
+#    plt.plot(design_index, gda_designs[i,:], "-")
+gda_designs = np.hstack(gda_designs)
+design_index = np.tile(design_index, len(output_GDA_list))
+plt.scatter(design_index, gda_designs, color='b', alpha=0.05)
+plt.xlabel('Observation Index')
+plt.ylabel('Time')
+plt.tight_layout()
+plt.savefig('plots/GDA_designs.pdf')
+
+plt.figure()
+pe_designs = np.hstack(final_design_GDA_list)
+plt.scatter(design_index, pe_designs, color='b', alpha=0.05)
+plt.xlabel('Observation Index')
+plt.ylabel('Time')
+plt.tight_layout()
+plt.savefig('plots/PE_designs.pdf')
---
+++ pk_posteriors.py
@@ -0,0 +1,110 @@
+import numpy as np
+import torch
+from torch.distributions import Normal, LogNormal, Independent
+import matplotlib.pyplot as plt
+import pickle
+
+################
+## IMPORT DESIGN
+################
+
+with open('pk_example.pkl', 'rb') as infile:
+    out_GDA, out_SGD = pickle.load(infile)
+
+output_GDA_list, final_design_GDA_list, pe_time_GDA_list = zip(*out_GDA)
+output_SGD_list, final_design_SGD_list, pe_time_SGD_list = zip(*out_SGD)
+
+design_adv = torch.tensor(final_design_GDA_list[0])
+design_fig = torch.tensor(final_design_SGD_list[0])
+
+#######################
+## PRIOR AND MODEL
+#######################
+
+loc = torch.tensor(np.log((0.1, 1., 20.)))
+scale = torch.tensor(np.sqrt((0.05, 0.05, 0.05)))
+prior = LogNormal(loc, scale)
+prior = Independent(prior, 1)
+
+def get_x(theta, design):
+    theta1 = theta[..., 0:1]
+    theta2 = theta[..., 1:2]
+    theta3 = theta[..., 2:3]
+    while design.dim() < theta1.dim():
+        design = design.unsqueeze(0)
+    x = 400. * theta2 * \
+        (torch.exp(-theta1*design) - torch.exp(-theta2*design)) \
+        / (theta3*(theta2-theta1))    
+    return x
+
+loc = torch.zeros(design_adv.shape)
+scale = 0.1 * torch.ones(design_adv.shape)
+noise = Normal(loc=loc, scale=scale)
+noise = Independent(noise, 1)
+
+#######################
+## GENERATE PSEUDO-DATA
+#######################
+
+torch.manual_seed(0)
+theta0 = prior.sample()
+noise0 = noise.sample()
+x0_adv = get_x(theta0, design_adv)
+x0_fig = get_x(theta0, design_fig)
+y0_adv = x0_adv + noise0
+y0_fig = x0_fig + noise0
+
+######################
+## IMPORTANCE SAMPLING
+######################
+
+def is_weights(theta, design, y0):
+    x = get_x(theta, design)
+    diff = x - y0.unsqueeze(0)
+    log_w = noise.log_prob(diff) - prior.log_prob(theta)
+    log_w -= torch.max(log_w)
+    w = torch.exp(log_w)
+    w /= torch.sum(w)
+    return w
+
+nsamples = 10 ** 7
+theta_is = prior.sample((nsamples,))
+w_adv = is_weights(theta_is, design_adv, y0_adv)
+w_fig = is_weights(theta_is, design_fig, y0_fig)
+
+#######
+## PLOT
+#######
+
+xdim = 0
+ydim = 2
+nplot = 500
+
+plt.figure()
+
+plt.plot(theta_is[0:5000, xdim], theta_is[0:5000, ydim], "go", alpha=0.3)
+
+tokeep = (w_adv > 1e-6)
+tt = theta_is[tokeep,:]
+ww = w_adv[tokeep]
+ww = ww.detach().numpy()
+ww /= np.sum(ww)
+ii = np.random.choice(len(ww), size=nplot, replace=True, p=ww)
+post = tt[ii,:]
+plt.plot(post[:, xdim], post[:, ydim], "b+", alpha=0.5)
+
+tokeep = (w_fig > 1e-6)
+tt = theta_is[tokeep,:]
+ww = w_fig[tokeep]
+ww = ww.detach().numpy()
+ww /= np.sum(ww)
+ii = np.random.choice(len(ww), size=nplot, replace=True, p=ww)
+post = tt[ii,:]
+plt.plot(post[:, xdim], post[:, ydim], "r^", alpha=0.5)
+
+plt.plot(theta0[xdim], theta0[ydim], "kx", markersize=10)
+
+plt.xlabel(r'$\theta_{:d}$'.format(xdim+1))
+plt.ylabel(r'$\theta_{:d}$'.format(ydim+1))
+plt.tight_layout()
+plt.savefig('plots/PK_posterior.pdf')
